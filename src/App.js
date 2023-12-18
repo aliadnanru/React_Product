054d0f2702ef0afbd24343b53af5c13b26a2a5c3
Ali <AlAliA@stud.kai.ru>
2023-12-19 01:32:34 +0300

    ddss

--- src/App.js
+++ src/App.js
@@ -9,17 +9,6 @@ import MySnackbar from "./Components/MySnackbar";
 import * as React from "react";
 import {AlertContext} from "./ProductContext/AlertContext";
 import SwipeableEdgeDrawer from "./Components/Swipeable edge";
-//mui
-import { styled } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Fab from '@mui/material/Fab';
-import MenuIcon from '@mui/icons-material/Menu';
-import AddIcon from '@mui/icons-material/Add';
-import SearchIcon from '@mui/icons-material/Search';
-import MoreIcon from '@mui/icons-material/MoreVert';
 // InitialProduct
 
 const InitialProduct = [{
@@ -43,7 +32,7 @@ function App() {
     const [openAlert, setOpenAlert] = useState(false);
     const [inpute, setinpute] = useState(InitiaInpute)
     const [product, setproduct] = useState(InitialProduct)
-    const [msg,setmsg]=useState("")
+    const [msg, setmsg] = useState("")
 
     function ShowAlert(msg) {
         setOpenAlert(true)
@@ -52,44 +41,21 @@ function App() {
             setOpenAlert(false)
         }, 2000)
     }
-    const StyledFab = styled(Fab)({
-        position: 'absolute',
-        zIndex: 1,
-        top: -30,
-        left: 0,
-        right: 0,
-        margin: '0 auto',
-    });
+
 
     return (
         <div className="App" style={{}}>
 
             <div className="App-header">
-                <AlertContext.Provider value={{openAlert, setOpenAlert ,ShowAlert}}>
+                <AlertContext.Provider value={{openAlert, setOpenAlert, ShowAlert}}>
                     <ProductContext.Provider value={{product, setproduct}}>
 
                         <InputeContext.Provider value={{inpute, setinpute}}>
                             <Mine/>
                             <Input_Btn/>
-                            {/*<SwipeableEdgeDrawer/>*/}
-                            <MySnackbar msg={[setmsg,msg]}/>
-                            <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
-                                <Toolbar>
-                                    <IconButton color="inherit" aria-label="open drawer">
-                                        <MenuIcon />
-                                    </IconButton>
-                                    <StyledFab color="secondary" aria-label="add">
-                                        <AddIcon />
-                                    </StyledFab>
-                                    <Box sx={{ flexGrow: 1 }} />
-                                    <IconButton color="inherit">
-                                        <SearchIcon />
-                                    </IconButton>
-                                    <IconButton color="inherit">
-                                        <MoreIcon />
-                                    </IconButton>
-                                </Toolbar>
-                            </AppBar>
+                            <SwipeableEdgeDrawer/>
+                            <MySnackbar msg={[setmsg, msg]}/>
+
                         </InputeContext.Provider>
 
                     </ProductContext.Provider>
